904794c932c6fe9b4e0fb6cf965b7bd187a9e802
mrp-v2 <matthrewp@gmail.com>
2024-05-02 09:03:36 -0600

    added extra info to cardinal directions about left and right relative directions

--- shared/src/main/java/chess/Direction.java
+++ shared/src/main/java/chess/Direction.java
@@ -3,18 +3,42 @@ package chess;
 public enum Direction {
     UP(-1, 0),
     DOWN(1, 0),
-    LEFT(0, -1),
-    RIGHT(0, 1),
+    LEFT(0, -1, DOWN, UP),
+    RIGHT(0, 1, UP, DOWN),
     UP_RIGHT(-1, 1),
     UP_LEFT(-1, -1),
     DOWN_RIGHT(1, 1),
     DOWN_LEFT(1, -1);
 
+    public static final Direction[] CARDINAL = new Direction[]{UP, LEFT, DOWN, RIGHT};
+
     public final int rowChange, columnChange;
+
+    private Direction left, right;
 
     Direction(int rowChange, int columnChange) {
         this.rowChange = rowChange;
         this.columnChange = columnChange;
+    }
+
+    Direction(int rowChange, int columnChange, Direction left) {
+        this(rowChange, columnChange);
+        this.left = left;
+        left.right = this;
+    }
+
+    Direction(int rowChange, int columnChange, Direction left, Direction right) {
+        this(rowChange, columnChange, left);
+        this.right = right;
+        right.left = this;
+    }
+
+    public Direction left() {
+        return left;
+    }
+
+    public Direction right() {
+        return right;
     }
 
     /**
