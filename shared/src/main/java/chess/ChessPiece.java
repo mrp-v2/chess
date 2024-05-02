725ae63a7584198a0eaf30edf1b5fc9f00b76c5d
mrp-v2 <matthrewp@gmail.com>
2024-05-02 08:34:03 -0600

    implemented pawn moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -52,6 +53,71 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        switch (type) {
+            case PAWN:
+                return pawnMoves(board, myPosition);
+            case ROOK:
+                break;
+            case KNIGHT:
+                break;
+            case BISHOP:
+                break;
+            case QUEEN:
+                break;
+            case KING:
+                break;
+        }
         throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Handles checking for pawn promotion before adding a move.
+     *
+     * @param start The starting position of the move
+     * @param end   The ending position of the move
+     * @param moves A collection to add the move(s) to
+     */
+    private void addPawnMove(ChessPosition start, ChessPosition end, Collection<ChessMove> moves) {
+        if (!end.move(color.forward).isValid()) {
+            for (PieceType type : PieceType.values()) {
+                if (type == PieceType.PAWN) {
+                    continue;
+                }
+                moves.add(new ChessMove(start, end, type));
+            }
+        }
+        moves.add(new ChessMove(start, end));
+    }
+
+    /**
+     * Handles the possible moves for a pawn.
+     */
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessPosition moveOne = myPosition.move(color.forward);
+        ChessPiece pieceOne = board.getPiece(moveOne);
+        // normal forward moves
+        if (pieceOne == null) {
+            addPawnMove(myPosition, moveOne, moves);
+            if (myPosition.move(color.forward, -1).isValid() && !myPosition.move(color.forward, -2).isValid()) {
+                ChessPosition moveTwo = myPosition.move(color.forward, 2);
+                ChessPiece pieceTwo = board.getPiece(moveTwo);
+                if (pieceTwo == null) {
+                    moves.add(new ChessMove(myPosition, moveTwo));
+                }
+            }
+        }
+        // diagonal capture moves
+        ChessPosition captureLeft = myPosition.move(color.forwardLeft);
+        ChessPiece pieceLeft = board.getPiece(captureLeft);
+        if (pieceLeft != null && pieceLeft.color != color) {
+            addPawnMove(myPosition, captureLeft, moves);
+        }
+        ChessPosition captureRight = myPosition.move(color.forwardRight);
+        ChessPiece pieceRight = board.getPiece(captureRight);
+        if (pieceRight != null && pieceRight.color != color) {
+            addPawnMove(myPosition, captureRight, moves);
+        }
+        return moves;
     }
 }
