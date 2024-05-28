fbab8d159c297f8216c18fda2f71321561a7d4c4
mrp-v2 <matthrewp@gmail.com>
2024-05-28 10:08:19 -0600

    created helper class for input handling

---
+++ client/src/main/java/ui/UserInputHandler.java
@@ -0,0 +1,45 @@
+package ui;
+
+import connection.ServerResponse;
+
+import java.util.Scanner;
+
+public abstract class UserInputHandler {
+
+    protected final Scanner scanner;
+    private final String exitCommand;
+
+    protected UserInputHandler(Scanner scanner, String exitCommand) {
+        this.scanner = scanner;
+        this.exitCommand = exitCommand;
+    }
+
+    public void run() {
+        String command;
+        while (true) {
+            command = scanner.nextLine();
+            if (exitCommand.equals(command)) {
+                break;
+            } else {
+                String[] args = command.split(" ");
+                if (args.length == 0) {
+                    printHelp();
+                } else {
+                    handleArgs(args);
+                }
+            }
+        }
+    }
+
+    protected abstract void printHelp();
+
+    protected abstract void handleArgs(String[] args);
+
+    protected void printError(ServerResponse<?> response) {
+        if (response.responseCode() == 0) {
+            System.out.println(response.responseMessage());
+        } else {
+            System.out.printf("Error %d: %s%n", response.responseCode(), response.responseMessage());
+        }
+    }
+}
