17ae9e8c28a811bf8386e9dbbbae35ebfbb57fb0
mrp-v2 <matthrewp@gmail.com>
2024-05-28 08:43:10 -0600

    setup starter code for phase 5

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -34,7 +34,6 @@ public class StandardAPITests {
     public static void init() {
         server = new Server();
         var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
 
         serverFacade = new TestServerFacade("localhost", Integer.toString(port));
 
