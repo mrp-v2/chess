31d6539774d6363d1b47acdef8b56722025689f8
mrp-v2 <matthrewp@gmail.com>
2024-05-28 10:09:05 -0600

    refactored Main to MainUI using UserInputHandler

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,52 +1,12 @@
-import chess.*;
+import ui.MainUI;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String command;
-        do {
-            command = scanner.nextLine();
-            String[] parts = command.split(" ");
-            if (parts.length == 0) {
-                printHelp();
-                continue;
-            }
-            switch (parts[0]) {
-                case "quit":
-                    break;
-                case "login":
-                    login(Arrays.copyOfRange(parts, 1, parts.length));
-                    break;
-                case "register":
-                    register(Arrays.copyOfRange(parts, 1, parts.length));
-                    break;
-                default:
-                    printHelp();
-                    break;
-            }
-        } while (!"quit".equals(command));
-    }
-
-    private static void printHelp() {
-        System.out.println("Help!");
-    }
-
-    private static void login(String[] args) {
-        if (args.length != 2){
-            printHelp();
-            return;
-        }
-        // TODO server facade login
-    }
-
-    private static void register(String[] args) {
-        if (args.length != 3){
-            printHelp();
-            return;
-        }
-        // TODO server facade register
+        MainUI ui = new MainUI(scanner);
+        ui.run();
     }
 }
---
+++ client/src/main/java/ui/MainUI.java
@@ -0,0 +1,70 @@
+package ui;
+
+import connection.ServerFacade;
+import connection.ServerResponse;
+import model.AuthResponse;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainUI extends UserInputHandler {
+
+    private static final String HELP = """
+                                       quit
+                                       help
+                                       login <username> <password>
+                                       register <username> <password> <email>""";
+
+    public MainUI(Scanner scanner) {
+        super(scanner, "quit");
+    }
+
+    @Override
+    protected void printHelp() {
+        System.out.println(HELP);
+    }
+
+    @Override
+    protected void handleArgs(String[] args) {
+        switch (args[0]) {
+            case "quit":
+                break;
+            case "login":
+                login(Arrays.copyOfRange(args, 1, args.length));
+                break;
+            case "register":
+                register(Arrays.copyOfRange(args, 1, args.length));
+                break;
+            default:
+                printHelp();
+                break;
+        }
+    }
+
+    private void login(String[] args) {
+        if (args.length != 2) {
+            printHelp();
+            return;
+        }
+        ServerResponse<AuthResponse> response = ServerFacade.login(args[0], args[1]);
+        handleAuthResponse(response);
+    }
+
+    private void register(String[] args) {
+        if (args.length != 3) {
+            printHelp();
+            return;
+        }
+        ServerResponse<AuthResponse> response = ServerFacade.register(args[0], args[1], args[2]);
+        handleAuthResponse(response);
+    }
+
+    private void handleAuthResponse(ServerResponse<AuthResponse> response) {
+        if (response.ok()) {
+            PostLoginUI postLoginUI = new PostLoginUI(scanner, response.data().username(), response.data().authToken());
+            postLoginUI.run();
+        } else {
+            printError(response);
+        }
+    }
+}
