35892fc941f9b4483b59b93d3ac2c8dae71c3968
mrp-v2 <matthrewp@gmail.com>
2024-05-30 08:10:31 -0600

    wrote tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,33 @@
 package client;
 
+import chess.ChessGame;
+import connection.ServerFacade;
+import connection.ServerResponse;
+import model.AuthResponse;
+import model.GameResponse;
+import model.GamesResponse;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String EMAIL = "email";
+    private static final String GAME_NAME = "game";
+    private static final String INVALID = "blah blah";
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
+        int port = server.run(0);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +35,94 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerFail() {
+        register();
+        ServerResponse<AuthResponse> response = facade.register(USERNAME, PASSWORD, EMAIL);
+        assertFalse(response.ok());
     }
 
+    @Test
+    void register() {
+        ServerResponse<AuthResponse> response = facade.register(USERNAME, PASSWORD, EMAIL);
+        assertTrue(response.ok());
+    }
+
+    @Test
+    void login() {
+        register();
+        ServerResponse<AuthResponse> response = facade.login(USERNAME, PASSWORD);
+        assertTrue(response.ok());
+    }
+
+    @Test
+    void loginFail() {
+        ServerResponse<AuthResponse> response = facade.login(USERNAME, PASSWORD);
+        assertFalse(response.ok());
+    }
+
+    @Test
+    void createGame() {
+        String authToken = facade.register(USERNAME, PASSWORD, EMAIL).data().authToken();
+        ServerResponse<GameResponse> response = facade.createGame(authToken, GAME_NAME);
+        assertTrue(response.ok());
+    }
+
+    @Test
+    void createGameFail() {
+        assertFalse(facade.createGame(INVALID, GAME_NAME).ok());
+    }
+
+    @Test
+    void getGames() {
+        String authToken = facade.register(USERNAME, PASSWORD, EMAIL).data().authToken();
+        ServerResponse<GamesResponse> response = facade.getGames(authToken);
+        assertTrue(response.ok());
+    }
+
+    @Test
+    void getGamesFail() {
+        assertFalse(facade.getGames(INVALID).ok());
+    }
+
+    @Test
+    void joinGame() {
+        String authToken = facade.register(USERNAME, PASSWORD, EMAIL).data().authToken();
+        int gameID = facade.createGame(authToken, GAME_NAME).data().gameID();
+        ServerResponse<?> response = facade.joinGame(authToken, gameID, ChessGame.TeamColor.WHITE);
+        assertTrue(response.ok());
+    }
+
+    @Test
+    void joinGameFailNoAuth() {
+        assertFalse(facade.joinGame(INVALID, 0, ChessGame.TeamColor.WHITE).ok());
+    }
+
+    @Test
+    void joinGameFailColorTaken() {
+        String authToken = facade.register(USERNAME, PASSWORD, EMAIL).data().authToken();
+        int gameID = facade.createGame(authToken, GAME_NAME).data().gameID();
+        assertTrue(facade.joinGame(authToken, gameID, ChessGame.TeamColor.WHITE).ok());
+        assertFalse(facade.joinGame(authToken, gameID, ChessGame.TeamColor.WHITE).ok());
+    }
+
+    @Test
+    void joinGameFailInvalidGame() {
+        String authToken = facade.register(USERNAME, PASSWORD, EMAIL).data().authToken();
+        assertFalse(facade.joinGame(authToken, -1, ChessGame.TeamColor.WHITE).ok());
+    }
+
+    @Test
+    void observeGame() {
+        // TODO
+        assertTrue(true);
+    }
+
+    @Test
+    @BeforeEach
+    @AfterEach
+    void clearNoError() {
+        ServerResponse<?> response = facade.clear();
+        assertTrue(response.ok());
+    }
 }
